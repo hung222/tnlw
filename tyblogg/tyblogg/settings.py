2a09aa09d7566aa95543d345df9f3be1b574d69d
hung222 <90371118+hung222@users.noreply.github.com>
2022-11-27 19:25:00 +0700

    Update settings.py

--- tyblogg/tyblogg/settings.py
+++ tyblogg/tyblogg/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-
+os.environ.get('KEY')
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
